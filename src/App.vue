bad8bdf12ac2d844e2af727abe72d0540b59c4ad
Arkar Min Tun <arkarmintun1@gmail.com>
2020-01-23 13:54:52 +0630

    AKMT: Form created

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,57 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer fixed v-model="drawer" app>
+      <v-list dense>
+        <router-link v-bind:to="{name:'Home'}" class="side_bar_link">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </router-link>
+        <router-link v-bind:to="{name:'Contact'}" class="side_bar_link">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>contact_mail</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Contact</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </router-link>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar color="indigo" dark fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Home</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn text v-bind:to="{name: 'AddMovie'}">Add Movie</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-content>
+      <v-container fluid>
+        <div id="app">
+          <router-view/>
+        </div>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
+import './assets/stylesheets/main.css';
+
 export default {
   name: 'App',
+  data: () => ({
+    drawer: null,
+  }),
+  props: {
+    source: String,
+  },
 };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Contact.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-layout>
+    this is contact
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+  data() {
+    return {
+      msg: 'Contact Page',
+    };
+  },
+};
+</script>
